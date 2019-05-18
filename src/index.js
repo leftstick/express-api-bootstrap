e76984137c5290da7a2f3777d738c69595207357
Howard.Zuo <leftstick@qq.com>
2019-05-18 19:17:03 +0800

    fix: expose ResponseError correctly

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const { withExpressApp } = require('./libs/scanner')
 const { logger } = require('./libs/logger')
-const { ResponseError } = require('./libs/responseError')
+const ResponseError = require('./libs/responseError')
 
-module.exports.withExpressApp = withExpressApp
-
-module.exports.logger = logger
-
-module.exports.ResponseError = ResponseError
+module.exports = {
+  withExpressApp,
+  logger,
+  ResponseError
+}
