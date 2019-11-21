355c59e226ca50fbe6afdcb036ebf4e981bb129b
Howard.Zuo <leftstick@qq.com>
2019-11-21 10:25:38 +0800

    refactor: remove unnecessary plugin

--- build/babel/registerBabel.js
+++ build/babel/registerBabel.js
@@ -24,25 +24,10 @@ module.exports = function(opts = {}) {
       ]
     ],
     plugins: [
-      require.resolve('@babel/plugin-syntax-dynamic-import'),
-      require.resolve('@babel/plugin-proposal-optional-catch-binding'),
       [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
       [require.resolve('@babel/plugin-proposal-class-properties'), { loose: true }],
       require.resolve('babel-plugin-parameter-decorator'),
 
-      require.resolve('@babel/plugin-proposal-export-namespace-from'),
-      require.resolve('@babel/plugin-proposal-export-default-from'),
-      [require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'), { loose: false }],
-      [require.resolve('@babel/plugin-proposal-optional-chaining'), { loose: false }],
-      [
-        require.resolve('@babel/plugin-proposal-pipeline-operator'),
-        {
-          proposal: 'minimal'
-        }
-      ],
-      require.resolve('@babel/plugin-proposal-do-expressions'),
-      require.resolve('@babel/plugin-proposal-function-bind'),
-      require.resolve('babel-plugin-macros'),
       require.resolve('./tspath-mapping-plugin')
     ],
     extensions: opts.extensions ? opts.extensions : ['.ts', '.js'],
