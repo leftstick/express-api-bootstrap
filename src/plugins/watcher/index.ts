386683ac77fbbaa584b260bbacd436ccd5763485
Howard.Zuo <leftstick@qq.com>
2019-12-10 17:00:15 +0800

    refactor: .bootrc.ts shall be watched as well

--- src/plugins/watcher/index.ts
+++ src/plugins/watcher/index.ts
@@ -17,12 +17,14 @@ export default () => {
         return
       }
       const srcDir = resolve(cwd(), 'src')
+      const bootConfigFile = resolve(cwd(), '.bootrc.ts')
 
       const debounceRestart = debounce((msg: string) => {
         ProcessCtrl.restart(msg)
       }, 800)
+
       chokidar
-        .watch(srcDir, { ignoreInitial: true })
+        .watch([srcDir, bootConfigFile], { ignoreInitial: true })
         .on('add', path => debounceRestart(`${path} added`))
         .on('addDir', path => debounceRestart(`Dir ${path} added`))
         .on('change', path => debounceRestart(`${path} changed`))
